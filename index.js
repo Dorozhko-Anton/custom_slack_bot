39d6f5ec5702b4129adf56f71285652f71072e68
DOROZHKO Anton <dorozhko.a@gmail.com>
2019-10-06 22:20:52 +0200

    add on start action

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 var SlackBot = require('slackbots');
 var request  = require("request")
+// var Twit     = require("twit")
 
 const envKey = process.env.CUSTOM_BOT_TOKEN
+const interval = process.env.INTERVAL * 1000
+const twitter_id = process.env.TWITTER_ID 
+
+// twitter interface 
+
+// const T = new Twit({
+//   consumer_key: process.env.CONSUMERKEY,
+//   consumer_secret: process.env.CONSUMERSECRET,
+//   access_token: process.env.ACCESSTOKEN,
+//   access_token_secret: process.env.ACCESSTOKENSECRET,
+//   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+// });
+
+// const getAccountsLastActivity = (id) => {
+
+//   return new Promise( (resolve) => {
+
+//     T.get('statuses/lookup', {
+//       id: ids
+//     }, (twitterErr, twitterData) => {
+
+//       if (twitterErr) {
+//         resolve(twitterErr);
+//       } else {
+//         resolve(twitterData);
+//       }
+
+//   }) 
+// }
+
+
 // create a bot
 var bot = new SlackBot({
     token: envKey, // Add a bot https://my.slack.com/services/new/bot and put the token 
@@ -33,3 +65,20 @@ bot.on("message", msg => {
   const postMessage = (message, user) => {
     bot.postMessage(user, message, { as_user: true })
   }
+
+bot.on("start", () => {
+
+  console.log("Start PR bot ...");
+
+  setInterval(() => {
+    // check twitter last message 
+
+    // print that information as message to #random channel
+    var params = {
+      icon_emoji: ':cat:'
+    };
+  
+    bot.postMessageToChannel('random', 'meow!', params);
+
+  }, interval)
+})
